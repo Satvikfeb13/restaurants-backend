a90cc25531f70d6bd22f9030c3501303f3a7be19
SatvikPatil <patilsatvik1324@gmail.com>
2026-01-02 16:55:05 +0530

    final

--- src/test/java/com/satvik/restaurantapp/RestaurantsBackendApplicationTests.java
+++ src/test/java/com/satvik/restaurantapp/RestaurantsBackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cdac;
+package com.satvik.restaurantapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Day16BackendApplicationTests {
+class RestaurantsBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
